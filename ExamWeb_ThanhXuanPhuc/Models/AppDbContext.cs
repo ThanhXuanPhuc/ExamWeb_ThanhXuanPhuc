7bc8b92714d36a0d9996b5763de37a79d693b784
ADMIN <ADMIN@PC346>
2025-06-11 08:02:10 +0700

    123

---
+++ ExamWeb_ThanhXuanPhuc/Models/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExamWeb_ThanhXuanPhuc.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> ops) : base(ops)
+        {
+
+        }
+        public DbSet<Phim> Phims { set; get; }
+    }
+}
--- ExamWeb_ThanhXuanPhuc/Startup.cs
+++ ExamWeb_ThanhXuanPhuc/Startup.cs
@@ -1,6 +1,8 @@
+using ExamWeb_ThanhXuanPhuc.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,7 @@ namespace ExamWeb_ThanhXuanPhuc
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<AppDbContext>(ops => ops.UseSqlServer("name=MSSQL"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
