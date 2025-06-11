3c4348e6c9114f8a8e5e35f02b1937a924f05aef
ADMIN <ADMIN@PC346>
2025-06-11 07:53:51 +0700

    Add project files.

---
+++ ExamWeb_ThanhXuanPhuc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ExamWeb_ThanhXuanPhuc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
